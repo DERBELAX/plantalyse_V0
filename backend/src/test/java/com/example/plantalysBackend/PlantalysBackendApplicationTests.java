b526bce033459ab06d8eb5abb53ffa0291df99f7
DERBELAX <164332911+DERBELAX@users.noreply.github.com>
2025-10-15 10:59:27 +0200

    wip: changements locaux avant merge

--- backend/src/test/java/com/example/plantalysBackend/PlantalysBackendApplicationTests.java
+++ backend/src/test/java/com/example/plantalysBackend/PlantalysBackendApplicationTests.java
@@ -1,15 +1,44 @@
 package com.example.plantalysBackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+<<<<<<< HEAD
 @SpringBootTest
 @ActiveProfiles("test")
+=======
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+
+@ActiveProfiles("test")
+@SpringBootTest(
+    classes = PlantalysBackendApplicationTests.TestConfig.class,
+    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+    properties = "spring.main.web-application-type=none"
+)
+>>>>>>> origin/feat/gitignore-docker
 class PlantalysBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @SpringBootConfiguration
+    @EnableAutoConfiguration(exclude = {
+            SecurityAutoConfiguration.class,
+            SecurityFilterAutoConfiguration.class,
+            UserDetailsServiceAutoConfiguration.class,
+            OAuth2ClientAutoConfiguration.class,
+            OAuth2ResourceServerAutoConfiguration.class,
+            MailSenderAutoConfiguration.class
+    })
+    static class TestConfig {
+       
+    }
 
+    @Test
+    void contextLoads() { }
 }
